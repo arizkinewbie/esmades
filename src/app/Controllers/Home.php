c77d51a1f2df4ed55fe6aa5e5b3e82fc27894d0f
IsyantoID <isyanto.id@gmail.com>
2024-02-28 16:35:45 +0700

    nambah file update.php buat test pull code

--- src/app/Controllers/Home.php
+++ src/app/Controllers/Home.php
@@ -14,7 +14,7 @@ class Home extends BaseController
     public function index()
     {
         $data = [
-            'title' => 'Beranda',
+            'title' => 'Saya Coba Edit Ya!',
             'subTitle' => '',
             'view' => $this->var['viewPath'] . 'index',
         ];
