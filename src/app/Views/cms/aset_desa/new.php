04981d497ca07f71886cb13fae5a693ec40bcd48
IsyantoID <isyanto.id@gmail.com>
2024-03-02 01:12:36 +0700

    create aset desa

--- src/app/Views/cms/aset_desa/new.php
+++ src/app/Views/cms/aset_desa/new.php
@@ -40,7 +40,7 @@
 
 <script>
 var settings = {
-    "url": "http://api.esmades.id:80/api/barang/select2",
+    "url": "https://api.esmades.id:80/api/barang/select2",
     "method": "GET",
     "timeout": 0,
     "headers": {
