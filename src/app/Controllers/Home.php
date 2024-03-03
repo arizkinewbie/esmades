15b0e8528e9d5d9af426f3953f316a6f7f17a2fd
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-03-03 21:30:55 +0700

    Added profil response to FE

--- src/app/Controllers/Home.php
+++ src/app/Controllers/Home.php
@@ -5,19 +5,49 @@ namespace App\Controllers;
 class Home extends BaseController
 {
     protected $var = [];
+    protected $apiDomain;
+    protected $token;
+
 
     public function __construct()
     {
         $this->var['viewPath'] = 'home/';
+        $this->apiDomain = getenv('API_DOMAIN');
     }
-    
+
     public function index()
     {
+        $session = session();
+        $url = getenv('API_DOMAIN') . "/api/auth/login";
         $data = [
-            'title' => 'Beranda',
-            'subTitle' => '',
-            'view' => $this->var['viewPath'] . 'index',
+            'email' => getenv('KLIEN_EMAIL'),
+            'password' => getenv('KLIEN_PASS'),
         ];
-        return $this->render($data);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+        $session->set('jwtToken', json_decode($response)->token);
+
+        curl_close($ch);
+
+        //Get Profile
+        $dataRequest = [
+            'method' => 'GET',
+            'api_path' => '/api/profil?desa_id=29198',
+        ];
+        $response = $this->request($dataRequest);
+        if ($response->getStatusCode() == 200) {
+            $result = json_decode($response->getBody(), true);
+            $data = [
+                'title' => 'Beranda | ' . getenv('APP_NAME'),
+                'view' => $this->var['viewPath'] . 'index',
+            ];
+            $data = array_merge($data, $result[0]);
+            return $this->render($data);
+        } else {
+            return $this->notFound('Halaman tidak ditemukan!');
+        }
     }
 }
