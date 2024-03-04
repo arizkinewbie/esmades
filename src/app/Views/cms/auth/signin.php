586a10f791cb604d6159864266e57f2716c23f2c
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-03-04 10:03:27 +0700

    changes meta desc

--- src/app/Views/cms/auth/signin.php
+++ src/app/Views/cms/auth/signin.php
@@ -6,8 +6,8 @@
     <meta charset="utf-8" />
     <title><?= isset($title) ? $title : 'E-SmaDes' ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
+    <meta content="Sign in to continue to E-SmaDes." name="description" />
+    <meta content="E-SmaDes" name="author" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="<?= base_url('dist/') ?>assets/images/favicon.ico">
 
