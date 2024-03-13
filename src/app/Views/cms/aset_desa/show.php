80e175fc088c239b7ea2213599f59eac842c5c72
IsyantoID <isyanto.id@gmail.com>
2024-03-13 22:22:25 +0700

    detail aset

--- src/app/Views/cms/aset_desa/show.php
+++ src/app/Views/cms/aset_desa/show.php
@@ -46,7 +46,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <img src="#" class="preview-image">
+                <img src="#" width="100%" class="preview-image">
             </div>
             <div class="modal-footer">
                 <a href="javascript:void(0);" class="btn btn-link link-success fw-medium material-shadow-none" data-bs-dismiss="modal"><i class="ri-close-line me-1 align-middle"></i> Close</a>
