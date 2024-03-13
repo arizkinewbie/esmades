cde90b139f7a69c7548795490291fae6a56ab2fb
IsyantoID <isyanto.id@gmail.com>
2024-03-13 22:20:15 +0700

    detail aset

--- src/app/Views/cms/aset_desa/show.php
+++ src/app/Views/cms/aset_desa/show.php
@@ -30,10 +30,34 @@
                 <?php if(!empty($files)): ?>
                     <?php $arrayFiles = json_decode($files, true); ?>
                     <?php foreach($arrayFiles as $row): ?>
-                        <img widht="80" height="80" src="<?= base_url('uploads/aset_desa/' . $row['filename']) ?>">
+                        <img widht="80" height="80" src="<?= base_url('uploads/aset_desa/' . $row['filename']) ?>" onclick="return showImage(this.src)">
                     <?php endforeach ?>
                 <?php endif ?>
             </td>
         </tr>
     </tbody>
 </table>
+
+<div class="modal fade modalShowImage" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-md">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myExtraLargeModalLabel">Lihat Gambar</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img src="#" class="preview-image">
+            </div>
+            <div class="modal-footer">
+                <a href="javascript:void(0);" class="btn btn-link link-success fw-medium material-shadow-none" data-bs-dismiss="modal"><i class="ri-close-line me-1 align-middle"></i> Close</a>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<script>
+    function showImage(src) {
+        $('.preview-image').attr('src', src);
+        $('.modalShowImage').modal('show');
+    }
+</script>
